ca0e8f21882c1c7b35a8961260cb273f4c985ca3
joey <joey@joeys-MacBook-Pro.local>
2023-07-13 20:25:40 +0700

    config alias and things

---
+++ src/app/_components/nav-bar.tsx
@@ -0,0 +1,13 @@
+import NavMenu from "@radix-ui/react-navigation-menu";
+
+const NavBar = () => {
+  return (
+    <NavMenu.Root>
+      <NavMenu.List>
+        <NavMenu.Item>test</NavMenu.Item>
+      </NavMenu.List>
+    </NavMenu.Root>
+  );
+};
+
+export default NavBar;
