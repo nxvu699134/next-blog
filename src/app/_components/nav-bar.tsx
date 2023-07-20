3a989794d563a074ead54748788e07a7bcab2566
Vu Nguyen <nxvu699134@gmail.com>
2023-07-21 02:48:31 +0700

    (ui): setup layout

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -1,12 +1,64 @@
-import NavMenu from "@radix-ui/react-navigation-menu";
+import { ComponentPropsWithoutRef } from "react";
+interface NavLinkProps extends ComponentPropsWithoutRef<"a"> {}
+
+const NavLink = (props: NavLinkProps) => {
+  const { href, children } = props;
+  return <a href={href}>{children}</a>;
+};
 
 const NavBar = () => {
   return (
-    <NavMenu.Root>
-      <NavMenu.List>
-        <NavMenu.Item>test</NavMenu.Item>
-      </NavMenu.List>
-    </NavMenu.Root>
+    <nav className="border-gray-200 dark:bg-gray-900 bg-light-background-muted">
+      <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="mr-3 h-8"
+            alt="Flowbite Logo"
+          />
+          <span className="dark:text-white self-center whitespace-nowrap text-2xl font-semibold">
+            Flowbite
+          </span>
+        </a>
+        <button
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="text-gray-500 hover:bg-gray-100 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm focus:outline-none focus:ring-2 md:hidden"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="h-5 w-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div className="w-full md:block md:w-auto" id="navbar-default">
+          <ul className="border-gray-100 bg-gray-50 dark:border-gray-700 dark:bg-gray-800 md:bg-white md:dark:bg-gray-900 mt-4 flex flex-col rounded-lg border p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0">
+            <li>
+              <NavLink href="">Home</NavLink>
+            </li>
+            <li>
+              <NavLink href="">Blog</NavLink>
+            </li>
+            <li>
+              <NavLink href="">Résumé</NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,108 @@
+import { PALETTE as p } from "./palette";
+const boxShadow = {
+  // light
+  input: "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+  card: "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+  dropdown: "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+  // dark
+  "dark-input": "0 1px 2px 0 rgb(0 0 0 / 0.05)",
+  "dark-card": "0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1)",
+  "dark-dropdown":
+    "0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)",
+};
+
+const borderRadius = {
+  small: "0.375rem",
+  default: "0.5rem",
+  full: "9999px",
+};
+
+const fontSize = {
+  label: ["0.75rem"],
+  default: ["0.875rem", { lineHeight: "1.25rem" }],
+  title: ["1.125rem", { lineHeight: "1.75rem" }],
+  metric: ["1.875rem", { lineHeight: "2.25rem" }],
+};
+
+const colors = {
+  // light mode
+  light: {
+    brand: {
+      faint: p.blue["50"], // blue-50
+      muted: p.blue["200"], // blue-200
+      subtle: p.blue["400"], // blue-400
+      DEFAULT: p.blue["500"], // blue-500
+      emphasis: p.blue["700"], // blue-700
+      inverted: "#ffffff", // white
+    },
+    background: {
+      muted: p.gray["50"], // gray-50
+      subtle: p.gray["100"], // gray-100
+      DEFAULT: "#ffffff", // white
+      emphasis: p.gray["700"], // gray-700
+    },
+    border: {
+      DEFAULT: p.gray["200"], // gray-200
+    },
+    ring: {
+      DEFAULT: p.gray["200"], // gray-200
+    },
+    content: {
+      subtle: p.gray["400"], // gray-400
+      DEFAULT: p.gray["500"], // gray-500
+      emphasis: p.gray["700"], // gray-700
+      strong: p.gray["900"], // gray-900
+      inverted: "#ffffff", // white
+    },
+  },
+  // dark mode
+  dark: {
+    brand: {
+      faint: "#0B1229", // custom
+      muted: p.blue["950"], // blue-950
+      subtle: p.blue["800"], // blue-800
+      DEFAULT: p.blue["500"], // blue-500
+      emphasis: p.blue["400"], // blue-400
+      inverted: p.gray["950"], // gray-950
+    },
+    background: {
+      muted: "#131A2B", // custom
+      subtle: p.gray["800"], // gray-800
+      DEFAULT: p.gray["900"], // gray-900
+      emphasis: p.gray["300"], // gray-300
+    },
+    border: {
+      DEFAULT: p.gray["800"], // gray-800
+    },
+    ring: {
+      DEFAULT: p.gray["800"], // gray-800
+    },
+    content: {
+      subtle: p.gray["600"], // gray-600
+      DEFAULT: p.gray["600"], // gray-600
+      emphasis: p.gray["200"], // gray-200
+      strong: p.gray["50"], // gray-50
+      inverted: "#000000", // black
+    },
+  },
+};
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+const config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: colors,
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      boxShadow: boxShadow,
+      borderRadius: borderRadius,
+      fontSize: fontSize,
     },
   },
   plugins: [],
-}
+};
+
+export default config;
