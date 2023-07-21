7ad18478bc527ba17fd06270b6402129712da657
Vu Nguyen <nxvu699134@gmail.com>
2023-07-22 04:51:16 +0700

    (feat): Add site info

---
+++ src/app/_constants/site-info.ts
@@ -0,0 +1,22 @@
+const siteInfo = {
+  siteUrl: "https://codelmao.dev",
+  siteName: "Code LMAO - Vu's personal website",
+  github: "github.com/nxvu699134",
+  facebook: "https://www.facebook.com/4Euyen58uan56u/",
+  linkedin: "https://www.linkedin.com/in/nxvu699134/",
+  twitter: "https://twitter.com/VNguynXun7",
+  email: "nxvu699134@gmail.com",
+  desc: "Vu's peronal website. I like coding. I like beauty. I like coding to make beautiful things.",
+  keywords: [
+    "vunx",
+    "Vu Nguyen Xuan",
+    "Nguyen Xuan Vu",
+    "Software Engineer",
+    "Web Developer",
+    "Front-end Developer",
+    "codelmao",
+    "code lmao",
+  ],
+};
+
+export default siteInfo;
