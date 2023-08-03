8438c1cd5e10b8ef06672781bb6b7cec62208783
Vu Nguyen <nxvu699134@gmail.com>
2023-08-03 19:48:05 +0700

    (feat): Add menu button and nav bar for small mobile

--- src/app/_components/menu-button.tsx
+++ src/app/_components/menu-button.tsx
@@ -1,9 +1,9 @@
-import { ComponentPropsWithoutRef, Dispatch, SetStateAction } from "react";
+import { ComponentPropsWithoutRef } from "react";
 import clsx from "clsx";
 import Icon from "@ui/icon";
 
 interface MenuButtonProps extends ComponentPropsWithoutRef<"button"> {
-  toggled: boolean;
+  toggled?: boolean;
 }
 
 const MenuButton = (props: MenuButtonProps) => {
@@ -13,7 +13,7 @@ const MenuButton = (props: MenuButtonProps) => {
       onClick={onClick}
       className={clsx(
         className,
-        "relative flex h-[64px] w-[64px] items-center justify-center rounded-[50%] rounded-tr-none bg-light-brand text-light-brand-inverted",
+        "relative flex h-[64px] w-[64px] items-center justify-center rounded-[50%] rounded-br-none bg-light-brand text-light-brand-inverted",
       )}
     >
       <Icon
@@ -21,7 +21,7 @@ const MenuButton = (props: MenuButtonProps) => {
         size={36}
         className={clsx(
           "absolute inset-0 m-auto transition-all duration-300",
-          toggled ? "translate-x-0 opacity-100" : "-translate-x-4 opacity-0",
+          !toggled ? "translate-x-0 opacity-100" : "-translate-x-4 opacity-0",
         )}
       />
       <Icon
@@ -29,7 +29,7 @@ const MenuButton = (props: MenuButtonProps) => {
         size={36}
         className={clsx(
           "absolute inset-0 m-auto transition-all duration-300",
-          !toggled ? "translate-x-0 opacity-100" : "-translate-x-4 opacity-0",
+          toggled ? "translate-x-0 opacity-100" : "translate-x-4 opacity-0",
         )}
       />
     </button>
--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { PropsWithChildren, Fragment, useState } from "react";
+import { PropsWithChildren, Fragment, useState, useCallback } from "react";
 import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
@@ -9,7 +9,7 @@ import MenuButton from "@components/menu-button";
 interface NavLinkProps extends PropsWithChildren<LinkProps> {}
 
 const NavLink = (props: NavLinkProps) => {
-  const { href, children } = props;
+  const { href, children, onClick } = props;
   const pathName = usePathname();
   const isActive =
     (pathName === "/" && pathName === href) ||
@@ -17,10 +17,13 @@ const NavLink = (props: NavLinkProps) => {
   return (
     <Link
       className={clsx(
-        "rounded-lg px-4 py-2 uppercase",
-        isActive && "bg-light-brand text-light-brand-inverted",
+        "block rounded-lg uppercase sm:px-4 sm:py-2",
+        !isActive && "max-sm:text-blue-300 sm:hover:text-light-content-subtle",
+        isActive && "text-light-brand-inverted sm:bg-light-brand",
+        "max-sm:px-16 max-sm:py-3.5 max-sm:text-2xl",
       )}
       href={href}
+      onClick={onClick}
     >
       {children}
     </Link>
@@ -29,30 +32,60 @@ const NavLink = (props: NavLinkProps) => {
 
 const NavBar = () => {
   const [toggled, setToggled] = useState<boolean>(false);
+
+  const closeMenu = useCallback(() => setToggled(false), []);
+
   return (
     <Fragment>
       <MenuButton
         toggled={toggled}
         onClick={() => setToggled((t) => !t)}
-        className="!fixed right-0 top-0 sm:hidden"
+        className="!fixed bottom-0 right-0 z-50 sm:hidden"
       />
-      <nav className="mx-auto hidden w-full max-w-5xl  sm:block sm:w-11/12 md:w-10/12 lg:w-full">
-        <div className="relative ml-auto h-[68px] rounded-t-big bg-light-background lg:w-fit lg:rounded-tl-none lg:pl-4 lg:pr-10 lg:pt-4">
+      <nav className="mx-auto w-full max-w-5xl sm:block sm:w-11/12 md:w-10/12 lg:w-full">
+        <div
+          className={clsx(
+            "fixed right-0 top-0 z-20 h-full w-[100vh] origin-bottom-right bg-light-brand transition-all duration-300 sm:hidden",
+            toggled
+              ? "visible translate-x-0 translate-y-0 scale-100 rounded-none"
+              : "invisible scale-0 rounded-l-[100%] rounded-tr-[100%]",
+          )}
+        />
+        <div
+          className={clsx(
+            "relative ml-auto sm:h-[68px] sm:rounded-t-big sm:bg-light-background lg:w-fit lg:rounded-tl-none lg:pl-4 lg:pr-10 lg:pt-4",
+            "max-sm:fixed max-sm:bottom-[64px] max-sm:left-0 max-sm:right-0 max-sm:z-20 max-sm:mx-auto max-sm:transition-all max-sm:duration-500",
+            toggled
+              ? "max-sm:translate-y-0 max-sm:opacity-100"
+              : "max-sm:translate-y-full max-sm:opacity-0",
+          )}
+        >
           <div
             className="absolute left-[-131px] top-0 hidden h-full w-[135px] lg:block"
             style={{
               backgroundImage: `url(${navEdgeImg.src})`,
             }}
           />
-          <ul className="flex h-full items-center justify-center space-x-1.5">
+          <ul
+            className={clsx(
+              "flex h-full flex-col items-center justify-center sm:flex-row sm:space-x-1.5",
+              "max-sm:space-y-6",
+            )}
+          >
             <li>
-              <NavLink href="/">About</NavLink>
+              <NavLink href="/" onClick={closeMenu}>
+                About
+              </NavLink>
             </li>
             <li>
-              <NavLink href="/resume">Résumé</NavLink>
+              <NavLink href="/resume" onClick={closeMenu}>
+                Résumé
+              </NavLink>
             </li>
             <li>
-              <NavLink href="/blog">Blog</NavLink>
+              <NavLink href="/blog" onClick={closeMenu}>
+                Blog
+              </NavLink>
             </li>
           </ul>
         </div>
