5def1fce513e894b384d6db6eb7e1eb7d0fb8941
Vu Nguyen <nxvu699134@gmail.com>
2023-08-03 15:35:46 +0700

    (feat): Simplify nav

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, Fragment, useState } from "react";
 import Link, { LinkProps } from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
 import navEdgeImg from "@assets/images/nav-edge.svg";
+import MenuButton from "@components/menu-button";
 
 interface NavLinkProps extends PropsWithChildren<LinkProps> {}
 
@@ -27,31 +28,36 @@ const NavLink = (props: NavLinkProps) => {
 };
 
 const NavBar = () => {
+  const [toggled, setToggled] = useState<boolean>(false);
   return (
-    <nav className="mx-auto hidden w-full max-w-5xl flex-wrap items-center justify-between sm:flex sm:w-11/12 md:w-10/12 lg:w-full">
-      <div
-        className="relative ml-auto h-[68px] w-full rounded-t-big bg-light-background lg:w-fit lg:rounded-tl-none lg:pl-4 lg:pr-10 lg:pt-4"
-        id="navbar-default"
-      >
-        <div
-          className="absolute left-[-131px] top-0 hidden h-full w-[135px] lg:block"
-          style={{
-            backgroundImage: `url(${navEdgeImg.src})`,
-          }}
-        />
-        <ul className="flex h-full items-center justify-center space-x-1.5">
-          <li>
-            <NavLink href="/">About</NavLink>
-          </li>
-          <li>
-            <NavLink href="/resume">Résumé</NavLink>
-          </li>
-          <li>
-            <NavLink href="/blog">Blog</NavLink>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <Fragment>
+      <MenuButton
+        toggled={toggled}
+        onClick={() => setToggled((t) => !t)}
+        className="!fixed right-0 top-0 sm:hidden"
+      />
+      <nav className="mx-auto hidden w-full max-w-5xl  sm:block sm:w-11/12 md:w-10/12 lg:w-full">
+        <div className="relative ml-auto h-[68px] rounded-t-big bg-light-background lg:w-fit lg:rounded-tl-none lg:pl-4 lg:pr-10 lg:pt-4">
+          <div
+            className="absolute left-[-131px] top-0 hidden h-full w-[135px] lg:block"
+            style={{
+              backgroundImage: `url(${navEdgeImg.src})`,
+            }}
+          />
+          <ul className="flex h-full items-center justify-center space-x-1.5">
+            <li>
+              <NavLink href="/">About</NavLink>
+            </li>
+            <li>
+              <NavLink href="/resume">Résumé</NavLink>
+            </li>
+            <li>
+              <NavLink href="/blog">Blog</NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Fragment>
   );
 };
 
