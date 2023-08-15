6ebce639530f1ea3bdbae51fa8c008e4c51cbcca
Vu Nguyen <nxvu699134@gmail.com>
2023-08-15 19:35:35 +0700

    (chore): simplify code

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import { compileMDX, MDXRemoteProps } from "next-mdx-remote/rsc";
 import rehypeSlug from "rehype-slug";
 import rehypePrettyCode, {
@@ -45,20 +44,18 @@ const BlogDetail = async (props: BlogDetailProps) => {
   const postData = getPost(params.slug, false);
   const { toc, content } = await useMDX(postData.content);
   return (
-    <Fragment>
-      <article className="prose prose-blog flex min-w-[100%] max-w-[100%] flex-col lg:flex-row">
-        <h1 className="lg:hidden">{postData.title}</h1>
-        <aside className="mb-8 flex w-full flex-col rounded-big bg-light-background-subtle/50 p-10 lg:-mb-10 lg:-ml-8 lg:-mt-10 lg:mr-8 lg:w-[280px] lg:min-w-[280px]">
-          <div className="sticky top-10 lg:opacity-40 lg:transition-opacity lg:hover:opacity-100">
-            {toc}
-          </div>
-        </aside>
-        <div>
-          <h1 className="hidden lg:block">{postData.title}</h1>
-          {content}
+    <article className="prose prose-blog flex min-w-[100%] max-w-[100%] flex-col lg:flex-row">
+      <h1 className="lg:hidden">{postData.title}</h1>
+      <aside className="mb-8 flex w-full flex-col rounded-big bg-light-background-subtle/50 p-10 lg:-mb-10 lg:-ml-8 lg:-mt-10 lg:mr-8 lg:w-[280px] lg:min-w-[280px]">
+        <div className="sticky top-10 lg:opacity-40 lg:transition-opacity lg:hover:opacity-100">
+          {toc}
         </div>
-      </article>
-    </Fragment>
+      </aside>
+      <div>
+        <h1 className="hidden lg:block">{postData.title}</h1>
+        {content}
+      </div>
+    </article>
   );
 };
 
